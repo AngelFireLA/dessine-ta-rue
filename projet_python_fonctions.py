7bad41fdf0b8c70bc2526e8c9ddeb4ef59369b82
AngelFireLA <55795775+AngelFireLA@users.noreply.github.com>
2022-09-30 09:51:30 +0200

    Add files via upload

--- projet_python_fonctions.py
+++ projet_python_fonctions.py
@@ -1,17 +1,21 @@
 from turtle import *
 from random import *
 import math
+from tkinter import *
 
 immeuble_x = 0
 immeuble_y = 0
-sol_y = -100
+sol_y = int(window_height()/4*-1)
 r = 0
 g = 0
 b = 0
 
+
+root = Tk()
+
 screen = Screen()
-screen.setup(width=600, height=600)
-screen._root.resizable(False, False)
+screen.setup(width=root.winfo_screenwidth(), height=root.winfo_screenheight())
+root.destroy()
 
 def couleur_aleatoire():
     colormode(255)
@@ -30,15 +34,17 @@ def sol(y):
 def immeuble(immeubles):
     for immeuble in range(1,immeubles):
         im_width = randint(125,175)
-        im_etages = 2
+        im_etages = randint(1,6)
         penup()
         couleur_aleatoire()
         color((int(r*(3/4)), int(g*(3/4)), int(b*(3/4))))
-        setx(-250)
-        pendown()
+        setx(int((window_width()/2*-1)+window_width()/20))
         angle = 0
         for etages in range(im_etages):
+            pendown()
+            print(etages)
             begin_fill()
+            color((int(r*(3/4)), int(g*(3/4)), int(b*(3/4))))
             fillcolor((r, g, b))
             setx(xcor()+im_width)
             sety(ycor() +100)
@@ -47,42 +53,70 @@ def immeuble(immeubles):
             if etages == 0:
                 end_fill()
                 porte(im_width)
-            if etages != im_etages-1:
-                setheading(90)
-                forward(100)
             end_fill()
             if etages != 0:
-                fenetres(randint(3,10), im_width)
+                fenetres(randint(2,5), im_width)
+            setheading(90)
+            forward(100)
+        toit(im_width)
 
 def fenetres(f_number, im_width):
     penup()
+    im_starting_x = xcor()
     sety(ycor()+30)
     setx(int(xcor()+(im_width*1/5)/f_number))
+    window_size = int((im_width*2.5/5)/f_number)
     for f in range(f_number):
         color((0,0,0))
         pendown()
         begin_fill()
-        setx(int(xcor()+(im_width*2.5/5)/f_number))
+        setx(int(xcor()+window_size))
         sety(ycor()+ 40)
-        setx(int(xcor()-(im_width*2.5/5)/f_number))
+        setx(int(xcor()-window_size))
         sety(ycor()-40)
-        setx(int(xcor()+(im_width*2.5/5)/f_number))
+        setx(int(xcor()+window_size))
         fillcolor((153,255,255))
         end_fill()
         penup()
-        f_type = randint(2,2)
+        f_type = randint(1,3)
         if f_type == 2:
             setx(xcor() - 1)
-            setx(int(xcor()-((im_width*2.5/5)/f_number/2)))
-            #setx(int(xcor()+(im_width*2.5/5)/f_number))
+            setx(int(xcor()-(window_size/2)))
             pendown()
             sety(ycor()+ 40)
             sety(ycor()- 20)
-            setx(int(xcor()-((im_width*2.5/5)/f_number/2)))
-            setx(int(xcor()+(im_width*2.5/5)/f_number))
+            setx(int(xcor()-(window_size/2)))
+            setx(int(xcor()+(window_size-1)))
             penup()
             sety(ycor()- 20)
+        if f_type == 3:
+            pendown()
+            setx(xcor() - 1)
+            setx(int(xcor()-((im_width*2.5/5)/f_number)))
+            for loop in range(2):
+                setx(int(xcor() + window_size/5))
+                sety(ycor()+ 40)
+                setx(int(xcor() + window_size/5))
+                sety(ycor()- 40)
+            penup()
         setx(int(xcor()+(im_width*1.5/5)/f_number))
+    setx(im_starting_x)
+    sety(ycor()-30)
+
+def toit(im_width):
+    color((0,0,0))
+    fillcolor((0,0,0))
+    t_type = randint(1,1)
+    im_starting_x = xcor()
+    pendown()
+    setx(int(xcor()-im_width/20))
+    if t_type ==1:
+        begin_fill()
+        sety(ycor() + 15)
+        setx(xcor() + im_width + int(2*(im_width/20)))
+        sety(ycor() - 15)
+
+
                 
             
                 
@@ -128,20 +162,20 @@ def herbe(y):
     penup()
     colormode(255)
     color((0,255,0))
-    setx(-300)
+    setx(xcor() - int((window_width()/2)))
     pendown()
     pensize(2)
     begin_fill()
     sety((window_height()*-1))
     setx(window_width())
     sety(sol_y)
-    setx(-300)
+    setx(int(window_width()*-1))
     end_fill()
 
     
 
 def ville():
-    speed(10)
+    speed(100)
     sol(sol_y)
     herbe(sol_y)
     immeuble(2)
